58a7cc541301b65118b4b610f969e9fc90284746
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-06-13 15:08:31 +0530

    Update SpringOAuth2Application.java

--- src/main/java/com/springOAuth2/SpringOAuth2Application.java
+++ src/main/java/com/springOAuth2/SpringOAuth2Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringOAuth2Application {
-
+//test
 	public static void main(String[] args) {
 		SpringApplication.run(SpringOAuth2Application.class, args);
 	}
